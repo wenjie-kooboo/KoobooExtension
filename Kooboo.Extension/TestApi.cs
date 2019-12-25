4727ea51458d5e0bd4763857f4323d680fae6e4a
wenjie <wenjie@kooboo.cn>
2019-12-25 11:24:47 +0800

    searchindex test, view test

--- Kooboo.Extension/TestApi.cs
+++ Kooboo.Extension/TestApi.cs
@@ -2,9 +2,12 @@
 //All rights reserved.
 using Kooboo.Api;
 using Kooboo.Sites.Extensions;
+using Kooboo.Sites.Render;
+using Kooboo.Sites.Render.Components;
 using Kooboo.Sites.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Kooboo.Extension
 {
@@ -17,12 +20,33 @@ namespace Kooboo.Extension
 
         public dynamic Index(ApiCall call)
         {
-            // call.WebSite.SiteDb();
             return new
             {
-                name = "hello",
-                age = 17
+                name = call.WebSite.DisplayName,
+                id = call.WebSite.Id
             };
+        }
+
+        public List<Sites.Repository.SearchResult> Search(ApiCall call)
+        {
+            var sitedb = call.WebSite.SiteDb();
+            var keyword = System.Net.WebUtility.UrlDecode(call.GetRequestValue("keywords"));
+            string tag = "style='font-weight:bold;'";
+            return sitedb.SearchIndex.Search(keyword, 99999, tag, call.Context);
+        }
+
+        public async Task<string> View(ApiCall call)
+        {
+            var setting = new ComponentSetting();
+            setting.TagName = "view";
+            setting.NameOrId = call.GetRequestValue("name");
+            var ViewComponent = Sites.Render.Components.Container.Get("view");
+            string viewresult = null;
+            if (ViewComponent != null)
+            {
+                viewresult = await ViewComponent.RenderAsync(call.Context, setting);
+            }
+            return viewresult;
         }
     }
 
